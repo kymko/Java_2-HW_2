afa16f0557418b7189af57e89468b8e7f17db39d
kymko <Kymkobek@gmail.com>
2021-03-05 22:07:19 +0300

    added return for each one

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,19 +19,23 @@ public class Main {
                 Kyrgyzstan kyrgyzstan = new Kyrgyzstan("Central Asia", 6523529, "Kyrgyzstan", "Besh-Barmak");
                 kyrgyzstan.print();
                 kyrgyzstan.nationalFlag();
-                break;
+
+                return kyrgyzstan;
+
             case "Kazakhstan":
                 Kazakhstan kazakhstan = new Kazakhstan("Asia", 18877128, "Kazakhstan", "Nur-Sultan");
                 kazakhstan.print();
                 kazakhstan.nationalFlag();
-                break;
+
+                return kazakhstan;
+
             case "Russia":
                 Russia russia = new Russia("Eurasia", 146810000, "Russia", "RUB");
                 russia.print();
                 russia.nationalFlag();
+
                 return russia;
         }
         return null;
-
     }
 }
